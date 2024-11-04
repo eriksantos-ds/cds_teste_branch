08586cab261bfe24542e673c29ffb91dd42c1b53
Erik Santos <erik.macedo@hotmail.com>
2024-11-03 23:10:16 -0300

    Adicionado funcao de coleta de dados

---
+++ main.py
@@ -0,0 +1,17 @@
+def gether_data():
+    n1 = int(input("Primeiro Valor: "))
+    n2 = int(input("Segundo Valor: "))
+
+    return n1, n2
+
+
+def main():
+    n1, n2 = gether_data()
+
+    print(n1*n2)
+    
+    return None
+
+
+if __name__ == "__main__":
+    main() 
