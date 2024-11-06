3a17297cfd1f520652bd7265214bcb2bea941b80
Erik Santos <erik.macedo@hotmail.com>
2024-11-05 21:55:14 -0300

    Criado funcao de exibicao de mensagem

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-def gether_data():
-    n1 = int(input("Primeiro Valor: "))
-    n2 = int(input("Segundo Valor: "))
+from pkgutil import get_data
 
-    return n1, n2
+
+def get_operation():
+    op = input("Operação: ")
+
+    return op
+
+def print_message(n1, n2):
+    print(f'Os valores {n1} e {n2} comados dão {n1+n2}')
+
+
+
+    return None
+
 
 
 def main():
-    n1, n2 = gether_data()
+    n1, n2 = get_data()
 
-    print(n1**n2)
+    print_message(n1, n2)
+
+    return None
+
+def main():
+    n1, n2 = get_data()
+
+    print_message(n1, n2)
 
     return None
 
